d6cc39cbb71a02b93cc7a76315551fbbdc2c8acf
nictene <142471161+nictene@users.noreply.github.com>
2023-09-03 14:11:34 -0500

    Add files via upload

---
+++ docs/Assignment1/printFirastNameAndLastUserInput.py
@@ -0,0 +1,7 @@
+#Creating an inmput field for user first and last name and print
+#start with creating a variable for both and first name and last name
+#print it, notice how the formatting of plain text opens with a double apostrophe and
+#closed with a double apostrophe
+firstName=input("Please enter your first name: ")
+lastName=input("Please enter your last name: ")
+print ("Howdy everybody, my first name is", firstName, "and my last name is", lastName)
