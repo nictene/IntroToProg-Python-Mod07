09635700519913e155fddda39ca1bcd33b774c13
nictene <142471161+nictene@users.noreply.github.com>
2023-09-03 14:12:42 -0500

    Add files via upload

---
+++ docs/Asssignment2/BasicMath.py
@@ -0,0 +1,59 @@
+#print("\t\t\t \\ \\ \\ \\")
+#print("\t\t\t \\ \\ \\ \\")
+#print("\t\t\t Nicole is awesome \\ \\ \\ \\")
+#print("\t\t\tNicole is awesome \\ \\ \\ \\")
+#print("\t\t\t Nicole is awesome \\ \\ \\ \\")
+#print("\t\t\tNicole is awesome \\\\\\\\")
+#print("\nNicole is awesome \\ \\ \\ \\")
+#print("\nNicole is awesome \\ \\ \\ \\")
+#print("\tNicole is awesome \\ \\ \\ \\")
+#print("Nicole who maybe awesome, Nicole \'the thinker', who thinks too much and should but she \"won\'t.\"")
+#print("My hair stylist, Henry \'The great', who never says \"can\'t.\"")
+#print("\a")
+#firstName=input("Please enter your first name: ")
+#lastName=input("Please enter your last name: ")
+#print ("Howdy there", firstName, lastName)
+#input("\n\nPress the enter key to exit.")firstName=input("Please enter your first name: ")
+#nicoleTimes=input("Please enter how many times you told nicole to slow down: ")
+#nicoleTimesNo=input("Please enter how many times she didn't: ")
+#nicoleTimesYes=input("Please enter how many times she did: ")
+#print ("Nicole was told to slow down a total of", nicoleTimes, "times",)
+#print ("Nicole didn't slow down a total of", nicoleTimesNo, "times",)
+#print ("Nicole DID slow down a total of", nicoleTimesYes, "times",)
+#input("\n\nPress the enter key to exit.")
+#below input
+#nicoleTimes=input("Please enter how many times you told nicole to slow down: ")
+#nicoleTimesNo=input("Please enter how many times she didn't: ")
+#nicoleTimesYes=input("Please enter how many times she did: ")
+#print ("Nicole was told to slow down a total of", nicoleTimes, "times",)
+#print ("Nicole didn't slow down a total of", nicoleTimesNo, "times",)
+#print ("Nicole DID slow down a total of", nicoleTimesYes, "times",)
+#input("\n\nPress the enter key to exit.")
+#below add subtract muliply and divide via user input
+#nicoleTimes=float(input("Please enter how many times you told nicole to slow down: "))
+#add
+#nicoleTimesNo=float(input("Please enter how many times Nicole did not slow down: "))
+#nicoleTimesYes=float(input("Please enter how many times Nicole did slow down: "))
+#result=nicoleTimesNo + nicoleTimesYes
+#print ("Nicole was told to slow down a total of", result, "times",)
+#input("\n\nPress the enter key to exit.")
+#subtract
+#nicoleTimes=float(input("Please enter how many times you told nicole to slow down: "))
+#nicoleTimesNo=float(input("Please enter how many times Nicole did not slow down: "))
+#result=nicoleTimes - nicoleTimesNo
+#print ("Nicole slowed down a total of", result, "times",)
+#input("\n\nPress the enter key to exit.")nicoleTimes=float(input("Please enter how many times you told nicole to slow down: "))
+#muliply
+#nicoleTimes=float(input("Please enter how many times you told Nicole to slow down last month: "))
+#nicoleTimesMonth=float(input("Please enter the number of months you would like to get an understanding of how many times she will be asked: "))
+#result=nicoleTimes * nicoleTimesMonth
+#print ("Nicole may be asked to slow down a total of", result, "times over a period of", nicoleTimesMonth, "months")
+#input("\n\nPress the enter key to exit.")
+#division
+nicoleTimes=float(input("Please enter how many times you told Nicole to slow down in the last 6 months: "))
+result=nicoleTimes / 6
+print ("Nicole may be asked to slow down a total of", result, "on average over a period of 1 month")
+input("\n\nPress the enter key to exit.")
+
+
+
